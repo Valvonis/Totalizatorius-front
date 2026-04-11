56df1a5f249819621464d0c677c3ca0f6fa18663
Dautartas <datismcs@gmail.com>
2026-04-11 14:55:56 +0300

    change BE url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://totalizatorius.herokuapp.com" });
+const API = axios.create({
+  baseURL: "totalizatorius-back-production.up.railway.app",
+});
 
 export const createMatch = (newMatch) => API.post("/matches", newMatch);
 export const fetchMatches = () => API.get("/matches");
